566dcf2e940bdd9240e0e456ae81447c1da5cc20
Tbread <81889764+Tbread@users.noreply.github.com>
2022-01-03 19:06:47 +0900

    Initial commit

---
+++ src/main/java/com/teamfresh/voc/model/VOC.java
@@ -0,0 +1,57 @@
+package com.teamfresh.voc.model;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+
+@NoArgsConstructor
+@Getter
+@Entity
+public class VOC extends Timestamped {
+    public enum responsibility {DRIVERFAULT, SALERFAULT}
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    //귀책이 운송사인지, 고객사인지
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private VOC.responsibility responsibility;
+
+    //배상금액
+    @Column(nullable = false)
+    private Long compensationAmount;
+
+    //귀책의 상세 내용
+    @NotEmpty
+    @Column(nullable = false)
+    private String faultDetails;
+
+    //운송사 귀책일시, 기사의 확인여부 (고객사 귀책일시, default값 true)
+    @Column(nullable = false)
+    private boolean driversChk;
+
+    //운송사 귀책일시, 기사의 이의제기 여부 (고객사 귀책일시, default값 false)
+    @Column(nullable = false)
+    private boolean objection;
+
+    //종료여부
+    @Column(nullable = false)
+    private boolean conclude;
+
+
+    @Builder
+    public VOC(responsibility responsibility, Long compensationAmount, String faultDetails) {
+        this.responsibility = responsibility;
+        this.compensationAmount = compensationAmount;
+        this.faultDetails = faultDetails;
+        this.objection = false;
+        this.conclude = false;
+        this.driversChk = false;
+        this.driversChk = !responsibility.toString().equals("DRIVERFAULT");
+    }
+}
